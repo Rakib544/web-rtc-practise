5807761eb0c5a25ba70bd54307dea34e02fc8603
Rakib544 <md.rakib10122003@gmail.com>
2021-10-24 11:50:30 +0600

    some component created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+import { AppBar, Typography } from "@material-ui/core";
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <AppBar position="static" color="inherit">
+        <Typography align="center" variant="h2">
+          Video Chat App
+        </Typography>
+      </AppBar>
+      {/* Video player */}
+      {/* Options => Notification */}
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/VideoPlater.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const VideoPlater = () => {
+  return <div>Video Player</div>;
+};
+
+export default VideoPlater;
